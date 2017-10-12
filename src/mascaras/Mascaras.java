3a89035731f6bb35033ce0d7d5563cb1a4ac8b2f
Alba Proyecto <Alba Proyecto@Alba_024>
2017-10-11 19:05:25 -0500

    Prueba Juego4

---
+++ src/mascaras/Mascaras.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mascaras;
+
+import java.text.ParseException;
+
+/**
+ *
+ * @author Alba Proyecto
+ */
+public class Mascaras {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws ParseException {
+        // TODO code application logic here
+        Frame f=new Frame();
+        f.setVisible(true);
+    }
+    
+}
